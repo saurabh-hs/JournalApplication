febb65a74bb7a37783a7984f342d561bf5d85d76
saurabh-hs <shshinde@mitaoe.ac.in>
2025-09-20 14:34:23 +0530

    JournalApp Backend Application completed

---
+++ journalApp/src/main/java/com/journalapp/journalApp/model/SentimentData.java
@@ -0,0 +1,15 @@
+package com.journalapp.journalApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SentimentData {
+    private String email;
+    private String sentiment;
+}
